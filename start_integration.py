82131c1c8e40e745c2f84f1d853c88f45d301fb3
Ilya Emelyanov <emel.9616@gmail.com>
2020-07-03 20:36:05 +0700

    IHub-158712 Add a file with Integration parameters to the integration directory

--- start_integration.py
+++ start_integration.py
@@ -2,8 +2,8 @@ import json
 
 from sms_notifservice import SmsNotifService
 
-with open('settings.json', "rb") as PFile:
-    pwd_data = json.loads(PFile.read().decode('utf-8'))
+with open('settings.json', "rb") as SFile:
+    pwd_data = json.loads(SFile.read().decode('utf-8'))
 
 ov_url = "https://" + pwd_data["oneVizionUrl"]
 ov_login = pwd_data["oneVizionLogin"]
@@ -18,8 +18,10 @@ awsRegion = pwd_data["awsRegion"]
 max_attempts = pwd_data["maxAttempts"]
 next_attempt_delay = pwd_data["nextAttemptDelay"]
 
-with open('ihub_process_id', "rb") as PFile:
-    process_id = PFile.read().decode('utf-8')
+with open('ihub_parameters.json', "rb") as PFile:
+    ihub_data = json.loads(PFile.read().decode('utf-8'))
+
+process_id = ihub_data['processId']
 
 notification_service = SmsNotifService(service_id, process_id, ov_url, ov_login, ov_pwd, phone_number_field,
                                        awsAccessKeyId, awsSecretAccessKey, awsRegion, max_attempts,
