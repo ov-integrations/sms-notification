3f36bf13552c7bc46098a62124891c7dd929fa84
Anastasiyaaa <piletsana@gmail.com>
2022-08-03 11:09:59 +0700

    IHub-216844 Remove useless intermediate assignment of settings file data to variables IHub-216844-96806 [DEV] Remove useless intermediate assignment of settings file data to variables

--- start_integration.py
+++ start_integration.py
@@ -7,28 +7,14 @@ import json
 from sms_notifservice import SmsNotifService
 
 with open('settings.json', "rb") as SFile:
-    pwd_data = json.loads(SFile.read().decode('utf-8'))
-
-ov_url = pwd_data["oneVizionUrl"]
-ov_login = pwd_data["oneVizionLogin"]
-ov_pwd = pwd_data["oneVizionPwd"]
-service_id = pwd_data["serviceId"]
-phone_number_field = pwd_data["phoneNumberField"]
-
-awsAccessKeyId = pwd_data["awsAccessKeyId"]
-awsSecretAccessKey = pwd_data["awsSecretAccessKey"]
-awsRegion = pwd_data["awsRegion"]
-
-max_attempts = pwd_data["maxAttempts"]
-next_attempt_delay = pwd_data["nextAttemptDelay"]
+    settings_data = json.loads(SFile.read().decode('utf-8'))
 
 with open('ihub_parameters.json', "rb") as PFile:
     ihub_data = json.loads(PFile.read().decode('utf-8'))
 
-process_id = ihub_data['processId']
-log_level = ihub_data['logLevel']
-
-notification_service = SmsNotifService(service_id, ov_url, ov_login, ov_pwd, phone_number_field,
-                                       awsAccessKeyId, awsSecretAccessKey, awsRegion, max_attempts,
-                                       next_attempt_delay, process_id, log_level)
+notification_service = SmsNotifService(settings_data["serviceId"], settings_data["oneVizionUrl"], 
+                                       settings_data["oneVizionLogin"], settings_data["oneVizionPwd"], settings_data["phoneNumberField"],
+                                       settings_data["awsAccessKeyId"], settings_data["awsSecretAccessKey"], settings_data["awsRegion"], 
+                                       settings_data["maxAttempts"], settings_data["nextAttemptDelay"], 
+                                       ihub_data['processId'], ihub_data['logLevel'])
 notification_service.start()
