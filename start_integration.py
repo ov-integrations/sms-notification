3d6940dca327d9a588581f46fc17f00261726f44
Anastasiyaaa <piletsana@gmail.com>
2022-08-04 22:15:50 +0700

    IHub-216844 Remove useless intermediate assignment of settings file data to variables IHub-216844-97053 [CR] Remove useless intermediate assignment of settings file data to variables

--- start_integration.py
+++ start_integration.py
@@ -1,20 +1,27 @@
 import subprocess
 import sys
 
-subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-r', 'python_dependencies.txt'])
+subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "python_dependencies.txt"])
 
 import json
 from sms_notifservice import SmsNotifService
 
-with open('settings.json', "rb") as SFile:
-    settings_data = json.loads(SFile.read().decode('utf-8'))
+with open("settings.json", "rb") as settings_file:
+    settings_json = json.loads(settings_file.read().decode("utf-8"))
 
-with open('ihub_parameters.json', "rb") as PFile:
-    ihub_data = json.loads(PFile.read().decode('utf-8'))
+with open("ihub_parameters.json", "rb") as ihub_params_file:
+    ihub_params_json = json.loads(ihub_params_file.read().decode("utf-8"))
 
-notification_service = SmsNotifService(settings_data["serviceId"], settings_data["oneVizionUrl"], 
-                                       settings_data["oneVizionLogin"], settings_data["oneVizionPwd"], settings_data["phoneNumberField"],
-                                       settings_data["awsAccessKeyId"], settings_data["awsSecretAccessKey"], settings_data["awsRegion"], 
-                                       settings_data["maxAttempts"], settings_data["nextAttemptDelay"], 
-                                       ihub_data['processId'], ihub_data['logLevel'])
+notification_service = SmsNotifService(settings_json["serviceId"],
+                                       settings_json["oneVizionUrl"], 
+                                       settings_json["oneVizionLogin"],
+                                       settings_json["oneVizionPwd"],
+                                       settings_json["phoneNumberField"],
+                                       settings_json["awsAccessKeyId"],
+                                       settings_json["awsSecretAccessKey"],
+                                       settings_json["awsRegion"], 
+                                       settings_json["maxAttempts"],
+                                       settings_json["nextAttemptDelay"], 
+                                       ihub_params_json["processId"],
+                                       ihub_params_json["logLevel"])
 notification_service.start()
